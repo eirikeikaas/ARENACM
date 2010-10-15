23b290a617e62789ccb971fedbb358f9e8867f94
Hogne <hogne@alwayshome.(none)>
2010-10-15 09:06:02 +0200

    English translation with support for localization
    Also added css styling elements.

--- web/templates/access_denied.php
+++ web/templates/access_denied.php
@@ -1,50 +1,52 @@
 <html>
 	<head>
 		<title>
-			Tilgang nektet
+			<?= i18n ( 'Please log in' ) ?>
 		</title>
 	</head>
-	<body>
-		<?if ( $_POST[ "webUsername" ] ) { ?>
-		<h1>
-			Innloggingen feilet!
-		</h1>
-		<p>
-			Innloggingen feilet. Vennligst prøv igjen.
-		</p>
-		<?}?>
-		<?if ( !$_POST[ "webUsername" ] ) { ?>
-		<h1>
-			Tilgang nektet!
-		</h1>
-		<p>
-			Siden du prøver å nå krever godkjenning!
-		</p>
-		<?}?>
+	<body class="Login">
+		<div class="Login">
+			<?if ( $_POST[ "webUsername" ] ) { ?>
+			<h1>
+				<?= i18n ( 'The username or password was wrong' ) ?>
+			</h1>
+			<p>
+				<?= i18n ( 'Please try to log in anew.' ) ?>
+			</p>
+			<?}?>
+			<?if ( !$_POST[ "webUsername" ] ) { ?>
+			<h1>
+				<?= i18n ( 'Please log in' ) ?>
+			</h1>
+			<p>
+				<?= i18n ( 'The page you are trying to reach requires user authentication.' ) ?>
+			</p>
+			<?}?>
 		
-		<form method="post">
-			<p>
-				<strong>Brukernavn:</strong>
-				<input type="text" name="webUsername" />
-			</p>
-			<p>
-				<strong>Passord:</strong>
-				<input type="password" name="webPassword" />
-			</p>
-			<p>
-				<button type="submit">
-					Logg inn
-				</button>
-			</p>
-		</form>
+			<form method="post">
+				<p>
+					<strong><?= i18n ( 'Username' ) ?>:</strong>
+					<input type="text" name="webUsername" />
+				</p>
+				<p>
+					<strong><?= i18n ( 'Password' ) ?>:</strong>
+					<input type="password" name="webPassword" />
+				</p>
+				<p>
+					<button type="submit">
+						<?= i18n ( 'Login' ) ?>
+					</button>
+				</p>
+			</form>
 		
-		<?if ( $_REQUEST[ "logout" ] ) { ?>
-		<p>
-			<a href="<?= BASE_URL ?>">&laquo; Til hovedsiden</a>
-		</p>
-		<?}?>
-		<?if ( !$_REQUEST[ "logout" ] ) { ?>
-		<?= $this->page->Parent > 0 ? ( '<p><a href="' . $this->parentPage->getUrl ( ) . '">' . i18n ( 'Go to parent page', $GLOBALS[ 'Session' ]->LanguageCode ) . '</a></p>' ) : '' ?>
-		<?}?>
+			<?if ( $_REQUEST[ "logout" ] ) { ?>
+			<p>
+				<a href="<?= BASE_URL ?>">&laquo; <?= i18n ( 'Go back' ) ?></a>
+			</p>
+			<?}?>
+			<?if ( !$_REQUEST[ "logout" ] ) { ?>
+			<?= $this->page->Parent > 0 ? ( '<p><a href="' . $this->parentPage->getUrl ( ) . '">' . i18n ( 'Go to parent page', $GLOBALS[ 'Session' ]->LanguageCode ) . '</a></p>' ) : '' ?>
+			<?}?>
+		</div>
 	</body>
 </html>
