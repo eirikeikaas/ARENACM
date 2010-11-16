e46489d6dbcd807adca11ea3702c190d6722b9a0
Hogne <hogne@alwayshome.(none)>
2010-11-16 10:57:24 +0100

    Fixed read more check on template

--- lib/skeleton/modules/mod_blog_overview/templates/web_blog_w_ingress.php
+++ lib/skeleton/modules/mod_blog_overview/templates/web_blog_w_ingress.php
@@ -4,7 +4,7 @@
 	</h2>
 	<p class="Block"><span class="Published"><?= i18n ( 'Published' ) ?></span> <span class="Date"><?= ArenaDate ( $this->blog->DateUpdated, DATE_FORMAT ) ?></span></p>
 	<div class="Block Leadin"><?= $this->blog->Leadin?></div>
-	<?if ( 1 ) { ?>
+	<?if ( $this->blog->Body ) { ?>
 	<p class="Block ReadMore"><a href="<?= $this->link ?>"><?= i18n ( 'Read more' ) ?></a></p>
 	<?}?>
 
