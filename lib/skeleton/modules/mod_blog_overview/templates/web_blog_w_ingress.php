27417ce823ceb5379ae1fc4ae3cd460dd412622d
Hogne <hogne@alwayshome.(none)>
2010-11-16 10:47:20 +0100

    Template for blog overview

---
+++ lib/skeleton/modules/mod_blog_overview/templates/web_blog_w_ingress.php
@@ -0,0 +1,10 @@
+
+	<h2 class="Block Title">
+		<?= $this->blog->Title ?>
+	</h2>
+	<p class="Block"><span class="Published"><?= i18n ( 'Published' ) ?></span> <span class="Date"><?= ArenaDate ( $this->blog->DateUpdated, DATE_FORMAT ) ?></span></p>
+	<div class="Block Leadin"><?= $this->blog->Leadin?></div>
+	<?if ( 1 ) { ?>
+	<p class="Block ReadMore"><a href="<?= $this->link ?>"><?= i18n ( 'Read more' ) ?></a></p>
+	<?}?>
+
