0383aef1fdda544b16d12bd7d1fe7e0ccaa0663f
Jolanda Engelvaart Titlestad <jollie@jolliesweetie.(none)>
2010-12-02 19:41:45 +0100

    Added proper licensing

--- lib/classes/template/ctemplate.php
+++ lib/classes/template/ctemplate.php
@@ -1,12 +1,27 @@
 <?php
-/*
-    Admin CORE - CMS and API
-    Copyright (C) 2006-2010 Hogne Titlestad
-    CopyRight (c) 2006-2010 eAdmin
-    
-    Email: hogga@sub-ether.org
-    Telephone: (+0047) 51 69 09 17
-*/
+ 
+
+/*******************************************************************************
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS"
+basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+License for the specific language governing rights and limitations
+under the License.
+
+The Original Code is (C) 2004-2010 Blest AS.
+
+The Initial Developer of the Original Code is Blest AS.
+Portions created by Blest AS are Copyright (C) 2004-2010
+Blest AS. All Rights Reserved.
+
+Contributor(s): Hogne Titlestad, Thomas Wollburg, Inge Jørgensen, Ola Jensen, 
+Rune Nilssen
+*******************************************************************************/
+
 class cTemplate
 {
 	var $data;
