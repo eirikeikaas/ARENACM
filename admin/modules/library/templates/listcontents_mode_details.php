e883e3feb63437ebaf16871c622bdc8577aca32c
Hogne <hogne@alwayshome.(none)>
2011-01-07 10:01:45 +0100

    Fixed column widths

--- admin/modules/library/templates/listcontents_mode_details.php
+++ admin/modules/library/templates/listcontents_mode_details.php
@@ -4,8 +4,8 @@
 			<th colspan="2"<?= $GLOBALS['Session']->LibraryListmode=='sortorder'?' class="Active"' : '' ?>><a href="admin.php?module=library&listmode=sortorder"><?= i18n ( 'Sortorder' ) ?>:</a></th>
 			<th<?= $GLOBALS['Session']->LibraryListmode=='title'?' class="Active"' : '' ?>><a href="admin.php?module=library&listmode=title"><?= i18n ( 'Title' ) ?>:</a></th>
 			<th<?= $GLOBALS['Session']->LibraryListmode=='filename'?' class="Active"' : '' ?>><a href="admin.php?module=library&listmode=filename"><?= i18n ( 'Filename' ) ?>:</a></th>
-			<th<?= $GLOBALS['Session']->LibraryListmode=='filesize'?' class="Active"' : '' ?>><a href="admin.php?module=library&listmode=filesize"><?= i18n ( 'Size' ) ?>:</a></th>
-			<th<?= $GLOBALS['Session']->LibraryListmode=='date'?' class="Active"' : '' ?>><a href="admin.php?module=library&listmode=date"><?= i18n ( 'Date modified' ) ?>:</a></th>
+			<th<?= $GLOBALS['Session']->LibraryListmode=='filesize'?' class="Active"' : '' ?> width="70"><a href="admin.php?module=library&listmode=filesize"><?= i18n ( 'Size' ) ?>:</a></th>
+			<th<?= $GLOBALS['Session']->LibraryListmode=='date'?' class="Active"' : '' ?> width="120"><a href="admin.php?module=library&listmode=date"><?= i18n ( 'Date modified' ) ?>:</a></th>
 		</tr>
 		<?= $this->contents ?>
 	</table>
