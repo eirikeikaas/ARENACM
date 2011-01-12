8c5561dae9cb34c399c11a7ac56dd47a5190c933
Hogne <hogne@alwayshome.(none)>
2011-01-12 13:59:41 +0100

    Proper click links

--- admin/templates/module_tab.php
+++ admin/templates/module_tab.php
@@ -1,8 +1,8 @@
 
-	<div class="ModuleTab<?= ( $this->active ? "Active" : "" ) ?>">
+	<div class="ModuleTab<?= ( $this->active ? "Active" : "" ) ?>" onclick="document.location='<?= $this->link ? ( 'admin.php?' . $this->link ) : ( BASE_URL . 'admin.php?module=' . $this->module ) ?>'">
 		<span>
 			<img src="admin/gfx/icons/<?= $this->image ?>" alt="module" width="16" height="16">
-			<a href="<?= $this->link ? ( 'admin.php?' . $this->link ) : ( BASE_URL . 'admin.php?module=' . $this->module ) ?>"><?= $this->moduleName ?></a>
+			<a href="javascript:void(0)"><?= $this->moduleName ?></a>
 		</span>
 	</div>
 
