c9f9f582c0502eacbaf85f9905e02fb374f90d81
Hogne <hogne@alwayshome.(none)>
2011-01-13 08:42:16 +0100

    Fixed clicking on module tabs

--- admin/templates/module_tab.php
+++ admin/templates/module_tab.php
@@ -1,8 +1,9 @@
 
-	<div class="ModuleTab<?= ( $this->active ? "Active" : "" ) ?>" onclick="document.location='<?= $this->link ? ( 'admin.php?' . $this->link ) : ( BASE_URL . 'admin.php?module=' . $this->module ) ?>'">
+	<div class="ModuleTab<?= ( $this->active ? "Active" : "" ) ?>">
 		<span>
-			<img src="admin/gfx/icons/<?= $this->image ?>" alt="module" width="16" height="16">
-			<a href="javascript:void(0)"><?= $this->moduleName ?></a>
+			<a href="<?= ( $this->link ? ( 'admin.php?' . $this->link ) : ( BASE_URL . 'admin.php?module=' . $this->module ) ) ?>">
+				<img src="admin/gfx/icons/<?= $this->image ?>" alt="module" width="16" height="16"> <?= $this->moduleName ?>
+			</a>
 		</span>
 	</div>
 
