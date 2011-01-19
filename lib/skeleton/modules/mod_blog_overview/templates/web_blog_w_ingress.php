cc93c275969220eff02d275a31bfd897cdf56fb5
Hogne <hogne@alwayshome.(none)>
2011-01-19 12:00:17 +0100

    Fixes

--- lib/skeleton/modules/mod_blog_overview/templates/web_blog_w_ingress.php
+++ lib/skeleton/modules/mod_blog_overview/templates/web_blog_w_ingress.php
@@ -7,7 +7,7 @@
 		<p class="Block"><span class="Published"><?= i18n ( 'Published' ) ?></span> <span class="Date"><?= ArenaDate ( $this->blog->DateUpdated, DATE_FORMAT ) ?></span></p>
 		<div class="Block Leadin"><?= $this->blog->Leadin?></div>
 		<?if ( $this->blog->Body ) { ?>
-		<p class="Block ReadMore"><a href="<?= $this->link ?>"><?= i18n ( 'Read more' ) ?></a></p>
+		<p class="Block ReadMore"><a href="<?= $this->link ?>"><span><?= i18n ( 'Read more' ) ?></span></a></p>
 		<?}?>
 	</div>
 
