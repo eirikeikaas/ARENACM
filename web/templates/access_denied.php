2c89eb4eff29de9f1ac66cff2f875f7d794d82fa
Hogne <hogne@alwayshome.(none)>
2011-01-25 10:27:35 +0100

    Added stylability

--- web/templates/access_denied.php
+++ web/templates/access_denied.php
@@ -7,32 +7,32 @@
 	<body class="Login">
 		<div class="Login">
 			<?if ( $_POST[ "webUsername" ] ) { ?>
-			<h1>
+			<h1 class="Login">
 				<?= i18n ( 'The username or password was wrong' ) ?>
 			</h1>
-			<p>
+			<p class="Please">
 				<?= i18n ( 'Please try to log in anew.' ) ?>
 			</p>
 			<?}?>
 			<?if ( !$_POST[ "webUsername" ] ) { ?>
-			<h1>
+			<h1 class="Login">
 				<?= i18n ( 'Please log in' ) ?>
 			</h1>
-			<p>
+			<p class="Authentication">
 				<?= i18n ( 'The page you are trying to reach requires user authentication.' ) ?>
 			</p>
 			<?}?>
 		
 			<form method="post">
-				<p>
+				<p class="Username">
 					<strong><?= i18n ( 'Username' ) ?>:</strong>
 					<input type="text" name="webUsername" />
 				</p>
-				<p>
+				<p class="Password">
 					<strong><?= i18n ( 'Password' ) ?>:</strong>
 					<input type="password" name="webPassword" />
 				</p>
-				<p>
+				<p class="Submit">
 					<button type="submit">
 						<?= i18n ( 'Login' ) ?>
 					</button>
@@ -40,12 +40,12 @@
 			</form>
 		
 			<?if ( $_REQUEST[ "logout" ] ) { ?>
-			<p>
+			<p class="Back">
 				<a href="<?= BASE_URL ?>">&laquo; <?= i18n ( 'Go back' ) ?></a>
 			</p>
 			<?}?>
 			<?if ( !$_REQUEST[ "logout" ] ) { ?>
-			<?= $this->page->Parent > 0 ? ( '<p><a href="' . $this->parentPage->getUrl ( ) . '">' . i18n ( 'Go to parent page', $GLOBALS[ 'Session' ]->LanguageCode ) . '</a></p>' ) : '' ?>
+			<?= $this->page->Parent > 0 ? ( '<p class="Parent"><a href="' . $this->parentPage->getUrl ( ) . '">' . i18n ( 'Go to parent page', $GLOBALS[ 'Session' ]->LanguageCode ) . '</a></p>' ) : '' ?>
 			<?}?>
 		</div>
 	</body>
