b9510217e616da78c03d2182ba94faed1bf1c7b1
m0ns00n <m0ns00n@Mynthol.(none)>
2011-07-11 11:28:25 +0200

    Fixed bug with date format

--- lib/skeleton/modules/mod_blog_overview/templates/web_blog_w_ingress.php
+++ lib/skeleton/modules/mod_blog_overview/templates/web_blog_w_ingress.php
@@ -4,7 +4,7 @@
 		<h2 class="Block Title">
 			<?= $this->blog->Title ?>
 		</h2>
-		<p class="Block"><span class="Published"><?= i18n ( 'Published' ) ?></span> <span class="Date"><?= ArenaDate ( $this->blog->DateUpdated, DATE_FORMAT ) ?></span></p>
+		<p class="Block"><span class="Published"><?= i18n ( 'Published' ) ?></span> <span class="Date"><?= ArenaDate ( DATE_FORMAT, $this->blog->DateUpdated ) ?></span></p>
 		<div class="Block Leadin"><?= $this->blog->Leadin?></div>
 		<?if ( $this->blog->Body ) { ?>
 		<p class="Block ReadMore"><a href="<?= $this->link ?>"><span><?= i18n ( 'Read more' ) ?></span></a></p>
