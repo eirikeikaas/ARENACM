4bcfe956bdecf353b7ad95449215c14f9e96b236
m0ns00n <m0ns00n@Mynthol.(none)>
2011-10-13 08:41:12 +0200

    Fix syntax error

--- web/index.php
+++ web/index.php
@@ -420,7 +420,7 @@ switch ( $_REQUEST[ 'mode' ] )
 					$document->doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
 					$document->docinfo = '<' . '?xml version="1.0"?' . '>'."\n";
 				}
-				if ( defined ( 'DOCTYPE_OVERRIDE' )
+				if ( defined ( 'DOCTYPE_OVERRIDE' ) )
 				{
 					$document->xmlns = '';
 					$document->doctype = DOCTYPE_OVERRIDE;
