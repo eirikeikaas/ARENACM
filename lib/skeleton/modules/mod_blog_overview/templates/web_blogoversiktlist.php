1800a88dc8b88abbec3add79e1a157d664bd5efc
m0ns00n <m0ns00n@m0ns00nEvo.(none)>
2012-04-13 12:37:30 +0200

    Fixed divider span

--- lib/skeleton/modules/mod_blog_overview/templates/web_blogoversiktlist.php
+++ lib/skeleton/modules/mod_blog_overview/templates/web_blogoversiktlist.php
@@ -1,4 +1,1 @@
-<?
-?>
-
-<div class="BlogRow"><a href="<?= $this->cnt->getRoute( ) . '/blogitem/' . $this->blog->ID . '_' . texttourl ( $this->blog->Title ) ?>.html"><?= $this->blog->Title ?></a><div class="BlogDate">- <?= $this->date ?></div></div>
+<div class="BlogRow"><a href="<?= $this->cnt->getRoute( ) . '/blogitem/' . $this->blog->ID . '_' . texttourl ( $this->blog->Title ) ?>.html"><?= $this->blog->Title ?></a><div class="BlogDate"><span class="BlogDivider">-</span> <?= $this->date ?></div></div>
